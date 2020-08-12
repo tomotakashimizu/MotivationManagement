04626f28ec2b370afdddb756440da4f84c067f21
tomotakashimizu <tomotaka.developer@gmail.com>
2020-08-12 22:37:41 +0900

    Initial Commit

---
+++ MotivationManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MotivationManagement
+//
+//  Created by 清水智貴 on 2020/08/12.
+//  Copyright © 2020 com.tomotaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
